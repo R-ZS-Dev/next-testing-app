bb716e7799720a5ff5a35279fab109502902cb19
R-ZS-Dev <ranazubair696@gmail.com>
2022-06-25 11:45:48 +0500

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Fragment } from 'react'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <Fragment>
+      Hello
+    </Fragment>
+  )
+}
